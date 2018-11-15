d3399061947415cc2bb111660e148f2c5a9d0212
Laura Felce <crepu83@yahoo.it>
2018-11-15 10:26:25 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,3 +9,11 @@ new_names  << "Hello, my name is #{name}."
 end
 return new_names
 end
+
+
+def assign_rooms (speakers)
+  new_array = []
+  speakers.each_with_index |speaker, index|
+  new_array << "Hello, #{speaker}! You'll be assigned to room #{index}!"
+end
+return new_array
