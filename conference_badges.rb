f4c995fc86601d6ecaab821c5d0a3b607de4db07
Laura Felce <crepu83@yahoo.it>
2018-11-15 09:50:22 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,6 @@
 def batch_badge_creator(names)
-new_names = []
 names.each do |name|
-  new_names = names.push("Hello, my name is #{name}.")
 end
-new_names
+return ("Hello, my name is #{name}.")
 end
+
