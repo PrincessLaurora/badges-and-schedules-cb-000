61bf0e5ef9aad5f6305b786c95fc04c4aa11d238
Laura Felce <crepu83@yahoo.it>
2018-11-15 11:13:45 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,29 +2,29 @@ def badge_maker(name)
 return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(names)
+def batch_badge_creator(name)
 new_names = []
-names.each do |name|
+name.each do |name|
 new_names  << "Hello, my name is #{name}."
 end
 return new_names
 end
 
 
-def assign_rooms(speakers)
+def assign_rooms(name)
   new_array = []
-  speakers.each_with_index do |speaker, index|
+  name.each_with_index do |speaker, index|
   new_array << "Hello, #{speaker}! You'll be assigned to room #{index + 1}!"
 end
 return new_array
 end
 
 
-def printer(attendees)
-batch_badge_creator(names).each do |name|
+def printer(name)
+batch_badge_creator(name).each do |name|
   puts name
 end
- assign_rooms(speakers).each do |room|
+ assign_rooms(name).each do |room|
 puts room
  end
  end
